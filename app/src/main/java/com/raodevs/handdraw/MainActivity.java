b6158efd2a191e6b16200ea19dcd17470ea79c53
Gaurav <gauravrao630@gmail.com>
2017-09-29 12:52:29 +0530

    added usage in demo project

--- app/src/main/java/com/raodevs/handdraw/MainActivity.java
+++ app/src/main/java/com/raodevs/handdraw/MainActivity.java
@@ -3,11 +3,17 @@ package com.raodevs.handdraw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.raodevs.touchdraw.TouchDrawView;
+
 public class MainActivity extends AppCompatActivity {
+
+    TouchDrawView touchDrawView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        touchDrawView = (TouchDrawView)findViewById(R.id.touch);
     }
 }
--- touchDraw/src/main/java/com/raodevs/touchdraw/TouchDrawView.java
+++ touchDraw/src/main/java/com/raodevs/touchdraw/TouchDrawView.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -19,8 +20,8 @@ public class TouchDrawView extends View {
 
     private Paint paint;
     private Path path;
-    private String text_color, bg_color;
-    private String text_size;
+    private String text_color="BLUE", bg_color="WHITE";
+    private String text_size="5f";
 
 
     public TouchDrawView(Context context, @Nullable AttributeSet attrs) {
@@ -28,29 +29,19 @@ public class TouchDrawView extends View {
         path = new Path();
         paint = new Paint();
         paint.setAntiAlias(true);
-        paint.setColor(Color.BLUE);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStyle(Paint.Style.STROKE);
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Canvas, 0, 0);
         initAttributes(typedArray);
-
-        if(text_color != null){
-            paint.setColor(Color.parseColor(text_color));
-        }
-        if(text_size != null){
-            paint.setStrokeWidth(Float.parseFloat(text_size));
-        }
-        if(bg_color!= null ){
-            this.setBackgroundColor(Color.parseColor(bg_color));
-        }
-
+        paint.setColor(Color.parseColor(text_color));
+        paint.setStrokeWidth(Float.parseFloat(text_size));
+        this.setBackgroundColor(Color.parseColor(bg_color));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawPath(path, paint);
-
     }
 
     @Override
@@ -74,14 +65,21 @@ public class TouchDrawView extends View {
         return true;
     }
 
-    public void clear(){
-        path.reset();
-    }
 
     private void initAttributes(TypedArray typedArray) {
-
-        text_color = typedArray.getString(R.styleable.Canvas_text_color);
-        text_size = typedArray.getString(R.styleable.Canvas_text_size);
-        bg_color = typedArray.getString(R.styleable.Canvas_bg_color);
+        String t_color = typedArray.getString(R.styleable.Canvas_paint_color);
+        String t_size = typedArray.getString(R.styleable.Canvas_paint_width);
+        String background_color = typedArray.getString(R.styleable.Canvas_bg_color);
+        if(t_color != null){
+            text_color = t_color;
+        }
+        if(t_size != null){
+            text_size = t_size;
+        }
+        if(background_color != null){
+            bg_color = background_color;
+        }
     }
+
+
 }
